af74a7dfa9290f04566d1047c827a6c6842993bc
Siddhikapatil <Siddhikampatil30@gmail.com>
2023-05-06 11:25:48 +0530

    half done

--- cnn.py
+++ cnn.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import tensorflow as tf
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, UpSampling2D, Input
+from keras.layers import Conv2D, MaxPooling2D, UpSampling2D, Flatten, Dense,Reshape, Input
 from keras.callbacks import ModelCheckpoint
 from skimage import img_as_float
 from skimage.restoration import denoise_nl_means, estimate_sigma, denoise_tv_chambolle
@@ -21,22 +21,30 @@ h = 0.2
 sigma = 0.5
 
 
-# denoised_img = non_local_means_denoise(img, h=h, sigma=sigma)
+denoised_img = non_local_means_denoise(img, h=h, sigma=sigma)
 
 # denoised_img = denoise_nl_means(img, h=1.15 * sigma, fast_mode=True, patch_size=5, patch_distance=3, multichannel=True)
 
 # Define the CNN model
 model = Sequential()
-model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=img.shape))
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(92, 92, 3)))
 model.add(MaxPooling2D((2, 2)))
 model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
 model.add(MaxPooling2D((2, 2)))
 model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+model.add(Flatten())
+
+model.add(Dense(67712))  # Reshape the tensor to (None, 67712)
+model.add(Reshape((92, 92, 32)))  # Reshape the tensor to (None, 92, 92, 32)
+
 model.add(UpSampling2D((2, 2)))
 model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
 model.add(UpSampling2D((2, 2)))
 model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
 model.add(Conv2D(3, (3, 3), activation='sigmoid', padding='same'))
+
+
+model.summary()
 
 # Compile the model
 model.compile(optimizer='adam', loss='mse')
@@ -49,13 +57,13 @@ model.fit(img, img, batch_size=32, epochs=50, validation_split=0.2, callbacks=[c
 model.load_weights('best_model.h5')
 
 # Resize the image to the expected input shape of the model
-img = resize(img, (225, 224, 3))
+img = resize(img, (92, 92, 3))
 
 # Add an extra dimension to represent the batch size
 img = np.expand_dims(img, axis=0)
 
 # Predict the denoised image using the model
-denoised_image = model.predict(img)
+denoised_image = model.predict(img.squeeze())
 
 # Save the denoised image
 imsave('denoised_image.jpg', denoised_image[0])
